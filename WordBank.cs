fe421e58257b4847a0e7d445cb6d71734d483b60
Candace Williams <candace.williams94@hotmail.com>
2021-09-10 19:00:10 -0400

    add word bank and set to choose randomly.

---
+++ WordBank.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hangman_Game
+{
+    public class WordBank
+    {
+        public List<string> Words = new List<string>()
+        {
+            "Programming",
+            "Engineering",
+            "Technicians",
+        };
+        public int WordCount { get; set; }
+
+        public string GetWord()
+        {
+            Random random = new Random();
+            int randomIndex = random.Next(0, WordCount);
+            return Words[randomIndex];
+        }
+    }
+}
