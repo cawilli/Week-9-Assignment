f757f777992a9655313e1b10015ddd7c943a9a1a
Candace Williams <candace.williams94@hotmail.com>
2021-09-11 19:29:28 -0400

    move main code to play HangMan into the HangMan class. Hang Man class code to show the letter selected.

--- HangMan.cs
+++ HangMan.cs
@@ -6,9 +6,34 @@ namespace Hangman_Game
 {
     public class HangMan
     {
-        public string PayingWord { get; set; }
+        //setting the initial variables for game 
+        //player will only get 6 attempts to play game
+        public bool[] letterSelected;
+        public string PlayingWord;
         public char Guess { get; set; }
         public string Misses { get; set; }
-        public int Attempts { get; set; }
+        public int Attempts = 6;
+        
+        public void Game()
+        {
+            WordBank RandomWord = new WordBank();
+            PlayingWord = RandomWord.GetWord();
+            letterSelected = new bool[RandomWord.WordCount];
+        }
+        public void ShowLetter()
+        {
+            for(int i = 0; i <PlayingWord.Length; i++)
+            {
+                if (letterSelected[i])
+                {
+                    Console.WriteLine(letterSelected[i]);
+                }
+                else
+                {
+                    Console.WriteLine("_");
+                }
+            }
+        }
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,27 +6,7 @@ namespace Hangman_Game
     {        
         static void Main(string[] args)
         {
-            Message.WelcomeMessage();
-            int attempts = 6;
-            while (attempts > 0)
-            {
-                var hangMan = new HangMan();
-                var options = new WordBank();
-                string word = options.GetWord();
-                
-                char guess = char.Parse(Console.ReadLine());
-                Console.WriteLine();
-
-                foreach (var letter in word)
-                {
-                    if (guess == letter)
-                    {
-                        hangMan.PayingWord = guess.ToString();
-                    }
-                    Console.WriteLine();
-                }
-
-            }
+            Message.WelcomeMessage();         
             
         }
     }
