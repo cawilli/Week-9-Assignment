47e1c9655a5128dac002c2f067b641f754c20dec
Candace Williams <candace.williams94@hotmail.com>
2021-09-14 13:58:22 -0400

    add if the game should end and adjust comments on code.

--- HangMan.cs
+++ HangMan.cs
@@ -15,6 +15,7 @@ namespace Hangman_Game
         public string Misses { get; set; }
         public int Attempts = 6;
         
+        //this will choose a word at random for the player to guess
         public void Game()
         {
             WordBank RandomWord = new WordBank();
@@ -36,8 +37,11 @@ namespace Hangman_Game
                 }
             }
         }
-        public string CorrectLetter()
+
+        // this will update which letters were found in the random word from the word bank created.
+        public string CorrectLetter(char letter)
         {
+            string pick = PlayingWord;
          for(int i = 0; i < PlayingWord.Length; i++)
             {
                 if (i == letter)
@@ -45,6 +49,28 @@ namespace Hangman_Game
                     letterSelected[i] = true;
                 }
             }
+        }
+        // this will track if the game should end
+        //taking in user input and comparing it to the length of the random word from the word bank
+        //if the user still has attempts it wil check the letter with the word
+        //if all the letters are guessed, ideally the game will end 
+        public bool EndGame()
+        {
+            int correctAnswer = 0;
+            if (Attempts < 1)
+                return true;
+
+            for (int i = 0; i <letterSelected.Length; i++)
+            {
+                if (!letterSelected[i])
+                    return false;
+                else correctAnswer++;
+            }
+
+            if (correctAnswer == PlayingWord.Length)
+                return true;
+            else
+                return false;
         }
 
     }
